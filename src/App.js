a18a38264bc1edfeff07ae1535090218708c2007
Alex Coder <aleks.coder@yandex.ru>
2024-11-16 17:11:57 +0300

    install and integrate React-Toolkit

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Home from "./pages/Home";
 import Not_Found from "./pages/Not-Found";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart";
+// import { useDispatch, useSelector } from "react-redux";
+// import { decrement, increment } from "./redux/slices/filterSlice";
 
-export const SearchContext = React.createContext('');
-
+export const SearchContext = React.createContext("");
 
 function App() {
-    const [searchValue, setSearchValue] = React.useState('');
+    const [searchValue, setSearchValue] = React.useState("");
+
+    // const filter = useSelector((state) => state.filter.value);
+    // const dispatch = useDispatch();
+
     return (
-        <SearchContext.Provider value={{searchValue, setSearchValue}}>
+        <SearchContext.Provider value={{ searchValue, setSearchValue }}>
             <div className="wrapper">
-                <Header
-                    
-                />
+                <Header />
                 <Routes>
-                    <Route
-                        path="/"
-                        element={<Home />}
-                    />
+                    <Route path="/" element={<Home />} />
                     <Route path="/cart" element={<Cart />} />
                     <Route path="*" element={<Not_Found />} />
                 </Routes>
